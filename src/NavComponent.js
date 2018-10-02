9ce616caf6da8f8859a6ef337c9930dfb206d882
Spencer James <spencerleejames@gmail.com>
2018-10-01 20:53:40 -0700

    Changed Nav anchor href's

--- src/NavComponent.js
+++ src/NavComponent.js
@@ -4,9 +4,9 @@ const NavComponent = (props) => {
     const { handleNavAddEmployee, handleNavViewEmployees } = props;
     return(
         <nav>
-            <a href="#" onClick={handleNavViewEmployees}>View Employees</a>
-            {'   '}
-            <a href="#" onClick={handleNavAddEmployee}>Add Employee</a>
+            <a href="javascript:;" onClick={handleNavViewEmployees}>View Employees</a>
+            {' '}
+            <a href="javascript:;" onClick={handleNavAddEmployee}>Add Employee</a>
         </nav>
     )
 }
