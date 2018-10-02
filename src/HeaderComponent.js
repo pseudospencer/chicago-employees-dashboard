72df085c2697f01fe79190e2c29eefec383f13e3
Spencer James <spencerleejames@gmail.com>
2018-10-02 12:32:10 -0700

    Added Header Component

---
+++ src/HeaderComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageHeader } from 'react-bootstrap';
+
+const HeaderComponent = (props) => {
+    return (
+        <PageHeader>City Of Chicago Employees Dashboard</PageHeader>
+    );
+}
+
+export default HeaderComponent;
